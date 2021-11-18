9c683dbb19954b71eae7347229ad07f334ab6942
Ajay Kumar <65783767+ajaykmr8684@users.noreply.github.com>
2021-11-19 00:00:35 +0530

    Add files via upload

---
+++ 23. Pattern-14.cpp
@@ -0,0 +1,40 @@
+/*
+
+PATTERN-14
+
+PRINT TABLE OF n
+
+
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= 10;  ++i) {
+        cout << n << "*" << i << "=" << n*i << endl;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
