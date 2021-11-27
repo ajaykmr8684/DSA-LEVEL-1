dacc9fb5397f557f2ebef3d4a1d3f75bcd66301a
Ajay Kumar <65783767+ajaykmr8684@users.noreply.github.com>
2021-11-28 01:00:33 +0530

    Add files via upload

---
+++ 38. SpanOfAnArray.cpp
@@ -0,0 +1,52 @@
+/*
+Span of an array.
+
+Span = max - min in the array.
+
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for (int i = 0; i < n; ++i ) {
+
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+
+    }
+
+    cout << max - min << endl;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
