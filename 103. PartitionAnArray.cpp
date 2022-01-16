35f92ac5c0fc39a5018d19b994e0fb988fbf4a9a
Ajay Kumar <65783767+ajaykmr8684@users.noreply.github.com>
2022-01-16 22:04:35 +0530

    Add files via upload

---
+++ 103. PartitionAnArray.cpp
@@ -0,0 +1,58 @@
+/*
+Partition an array using pivot
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+void swap(int arr[], int i, int j) {
+	cout << "Swapping " << arr[i] << " and " << arr[j] << endl;
+	int temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+}
+
+void partition(int arr[], int n, int pivot) {
+	// write your code
+
+
+	int i = 0;
+	int j = 0;
+	while (i < n) {
+		if (arr[i] > pivot) {
+			i++;
+		}  else {
+			swap(arr, i, j);
+			i++;
+			j++;
+		}
+	}
+
+}
+
+void print(int arr[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	int pivot;
+	cin >> pivot;
+
+	partition(arr, n, pivot);
+	print(arr, n);
+}
