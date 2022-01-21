36092e7131d077d061b4b31dcd5861696cc0a16a
Ajay Kumar <65783767+ajaykmr8684@users.noreply.github.com>
2022-01-21 21:58:00 +0530

    Add files via upload

---
+++ 108. TargetSumPair.cpp
@@ -0,0 +1,52 @@
+/*
+Target Sum Pair
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void input(vector<int>& arr)
+{
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cin >> arr[i];
+	}
+}
+
+
+
+
+void targetSumPair(vector<int>& arr, int target)
+{
+	//write your code here
+
+	sort(arr.begin(), arr.end());
+
+	int i = 0;
+	int j = arr.size() - 1;
+
+	while (i < j) {
+		if (arr[i] + arr[j] == target) {
+			cout << arr[i] << ", " << arr[j] << endl;
+			i++;
+			j--;
+
+		} else if (arr[i] + arr[j] > target) {
+			j--;
+		} else {
+			i++;
+		}
+	}
+}
+int main()
+{
+	int n, target;
+	cin >> n;
+	vector<int> vec(n, 0);
+	input(vec);
+	cin >> target;
+	targetSumPair(vec, target);
+	return 0;
+}
