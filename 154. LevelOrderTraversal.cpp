b58f90d6fcfd98151cd0958ed29e74095fa427c2
Ajay Kumar <65783767+ajaykmr8684@users.noreply.github.com>
2022-04-02 21:31:25 +0530

    Add files via upload

---
+++ 154. LevelOrderTraversal.cpp
@@ -0,0 +1,66 @@
+/*
+
+Level order Traversal of a BT recursive
+
+Logic is simple: Remove print add - RPA
+
+1. Remove the front from Queue
+2. Print that element
+3. add its left and right elements if there are any.
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+	vector<vector<int>> levelOrder(TreeNode* root) {
+		vector<vector<int>> ans;
+		if (root == NULL) {
+			return ans;
+		}
+
+		queue<TreeNode*> q;
+		q.push(root);
+
+		while (!q.empty()) {
+			vector<int> level;
+			int size = q.size();
+
+			for (int i = 0; i < size; ++i) {
+				TreeNode *node = q.front();
+				q.pop();
+
+				if (node->left != NULL) {
+					q.push(node -> left);
+				}
+
+				if (node->right != NULL) {
+					q.push(node -> right);
+				}
+
+				level.push_back(node->val);
+			}
+
+			ans.push_back(level);
+
+		}
+
+		return ans;
+
+	}
+};
