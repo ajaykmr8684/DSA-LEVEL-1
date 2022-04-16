94b01c7ab2a3d3da96caa3eccdebd4a06a664513
Ajay Kumar <65783767+ajaykmr8684@users.noreply.github.com>
2022-04-16 23:07:58 +0530

    Add files via upload

---
+++ 159. MaxDepthBTIterative.cpp
@@ -0,0 +1,57 @@
+/*
+
+Maximum Depth of BT Iterative
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+	int maxDepth(TreeNode* root) {
+
+
+		if (root == NULL) {
+			return 0;
+		}
+
+		queue<TreeNode*> q;
+
+		q.push(root);
+		int depth = 0;
+
+		while (!q.empty()) {
+			depth++;
+			int size = q.size();
+
+			for (int i = 0; i < size; ++i) {
+				TreeNode *node = q.front();
+				q.pop();
+
+				if (node->left != NULL) {
+					q.push(node->left);
+				}
+
+				if (node->right != NULL) {
+					q.push(node->right);
+				}
+
+			}
+		}
+
+		return depth;
+
+	}
+};
